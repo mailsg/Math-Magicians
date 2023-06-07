107e291b17b02e895faf30c49cab9b0dde88e957
Sandeep Ghosh <mailsg0912@gmail.com>
2023-06-07 16:16:00 +0530

    Add event to the buttons for calculator operation

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,50 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
+import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="container">
-    <div className="calculator">
-      <div className="screen">
-        <input type="text" value="0" readOnly className="screen-color" />
+const Calculator = () => {
+  const [calculatorState, setCalculatorState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonValue) => {
+    const updatedState = calculate(calculatorState, buttonValue);
+    setCalculatorState(updatedState);
+  };
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <div className="screen">
+          <input type="text" value={calculatorState.next || calculatorState.total || '0'} readOnly className="screen-color" />
+        </div>
+        <div className="buttons">
+          <div className="row-one">
+            <Button value="AC" className="" handleButtonClick={handleButtonClick} />
+            <Button value="+/-" className="" handleButtonClick={handleButtonClick} />
+            <Button value="%" className="" handleButtonClick={handleButtonClick} />
+            <Button value="÷" className="orange-color" handleButtonClick={handleButtonClick} />
+          </div>
+          <div className="row-two">
+            <Button value="7" className="" handleButtonClick={handleButtonClick} />
+            <Button value="8" className="" handleButtonClick={handleButtonClick} />
+            <Button value="9" className="" handleButtonClick={handleButtonClick} />
+            <Button value="x" className="orange-color" handleButtonClick={handleButtonClick} />
+          </div>
+          <div className="row-three">
+            <Button value="4" className="" handleButtonClick={handleButtonClick} />
+            <Button value="5" className="" handleButtonClick={handleButtonClick} />
+            <Button value="6" className="" handleButtonClick={handleButtonClick} />
+            <Button value="-" className="orange-color" handleButtonClick={handleButtonClick} />
+          </div>
+          <div className="row-four">
+            <Button value="1" className="" handleButtonClick={handleButtonClick} />
+            <Button value="2" className="" handleButtonClick={handleButtonClick} />
+            <Button value="3" className="" handleButtonClick={handleButtonClick} />
+            <Button value="+" className="orange-color" handleButtonClick={handleButtonClick} />
+          </div>
+          <div className="row-five">
+            <Button value="0" className="big" handleButtonClick={handleButtonClick} />
+            <Button value="." className="" handleButtonClick={handleButtonClick} />
+            <Button value="=" className="orange-color" handleButtonClick={handleButtonClick} />
+          </div>
+        </div>
       </div>
-      <Button />
     </div>
-  </div>
+  );
+};
+
+const Button = ({ value, className, handleButtonClick }) => (
+  <input
+    type="button"
+    value={value}
+    className={className}
+    onClick={() => handleButtonClick(value)}
+  />
 );
 
-const Button = () => (
-  <div className="buttons">
-    <div className="row-one">
-      <input type="button" value="AC" />
-      <input type="button" value="+/-" />
-      <input type="button" value="%" />
-      <input type="button" value="/" className="orange-color" />
-    </div>
-    <div className="row-two">
-      <input type="button" value="7" />
-      <input type="button" value="8" />
-      <input type="button" value="9" />
-      <input type="button" value="*" className="orange-color" />
-    </div>
-    <div className="row-three">
-      <input type="button" value="4" />
-      <input type="button" value="5" />
-      <input type="button" value="6" />
-      <input type="button" value="-" className="orange-color" />
-    </div>
-    <div className="row-four">
-      <input type="button" value="1" />
-      <input type="button" value="2" />
-      <input type="button" value="3" />
-      <input type="button" value="+" className="orange-color" />
-    </div>
-    <div className="row-five">
-      <input type="button" value="0" className="big" />
-      <input type="button" value="." />
-      <input type="button" value="=" className="orange-color" />
-    </div>
-  </div>
-);
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  handleButtonClick: PropTypes.func.isRequired,
+};
 
 export default Calculator;
